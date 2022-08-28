54b2c0dff0206c1922aa46abe3da65d694729c06
unknown <wnichols1865@gmail.com>
2022-08-27 20:32:25 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { JokeComponent } from './joke/joke.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { JokePageComponent } from './joke-page/joke-page.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'jokepage', component: JokePageComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JokeComponent,
+    HomeComponent,
+    JokePageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatCardModule,
+    MatCheckboxModule,
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/joke/joke.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { JokeService } from './joke.service';
+@Component({
+  selector: 'app-joke',
+  templateUrl: './joke.component.html',
+  styleUrls: ['./joke.component.css']
+})
+export class JokeComponent implements OnInit {
+
+  joke$?: Observable<any>;
+  jokes$?: Observable<any[]>;
+
+  categories: any[] = [
+    { name: 'Programming', isChecked: true },
+    { name: 'Misc', isChecked: false },
+    { name: 'Dark', isChecked: false },
+    { name: 'Pun', isChecked: false },
+    { name: 'Spooky', isChecked: false },
+    { name: 'Christmas', isChecked: false }
+  ];
+
+  constructor(public jokeService: JokeService) {
+  }
+
+  trackByFn(item: Object) { return item; }
+
+  getJoke(): void {
+    this.joke$ = this.jokeService.getJoke(this.categories);
+  }
+
+  getJokes(): void {
+    this.jokes$ = this.jokeService.getJokes();
+    console.log("=========\n" + this.jokes$);
+  }
+
+  ngOnInit(): void {
+    this.getJoke();
+  }
+
+
+
+}
