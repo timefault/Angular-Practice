62b773d7d850efb2b90a74ff91a59156aa9a5498
unknown <wnichols1865@gmail.com>
2022-08-28 21:48:08 -0400

    using html params

--- src/app/components/weather-page/weather-page.component.ts
+++ src/app/components/weather-page/weather-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { WeatherService } from 'src/app/services/weather.service';
 
 @Component({
   selector: 'app-weather-page',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./weather-page.component.css']
 })
 export class WeatherPageComponent implements OnInit {
+  weather$?: Observable<any>;
+  weatherData: any;
 
-  constructor() { }
+  lat = 33.52668453600432;
+  lon = -81.83561253589173;
+
+  constructor(public weatherService: WeatherService) { }
 
   ngOnInit(): void {
+    this.getWeather();
+  }
+  getWeather() {
+    this.weather$ = this.weatherService.getWeatherDataByCoord(this.lat, this.lon);
+    this.weather$?.subscribe(data => {
+      this.weatherData = data;
+      console.log(this.weatherData.weather[0].main)
+    });
   }
 
 }
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,26 +1,30 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  REST_API = 'https://api.openweathermap.org/data/3.0/onecall';
+  REST_API = 'https://api.openweathermap.org/data/2.5/weather';
   headers = new HttpHeaders().set(
     'Content-Type',
     'application/json'
   );
   // geo data
-  lat = 33.52668453600432;
-  lon = -81.83561253589173;
   // api key
   API_KEY = 'bf4d44fa559cfb3b741788bdce242c8d';
   // units
   units = 'imperial';
   constructor(public httpClient: HttpClient) { }
 
-  getWeather() {
-    return this.httpClient.get(`${this.REST_API}?lat=${lat}&lon=${lon}&appid=${this.API_KEY}`);
+  getWeatherDataByCoord(lat: number, lon: number): any {
+    let params = new HttpParams()
+      .set('lat', lat)
+      .set('lon', lon)
+      .set('units', 'imperial')
+      .set('appid', this.API_KEY);
+
+    return this.httpClient.get(this.REST_API, { params });
   }
 }
